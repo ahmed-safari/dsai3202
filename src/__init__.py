5e1f4a66a52f5b0a0a183e756ca8f4951164c0e0
student <student@dsai3203-template.udst.internal>
2024-02-06 09:47:16 +0300

    initial commit

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+
+# Write a Python program that calculates the sum of all numbers from 1 to a
+# given large number .
+# b. Measure the execution time of the program using the time module.
+# c. Print the sum and the execution time (Put them in variables).
